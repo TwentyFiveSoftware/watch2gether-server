f59f274c4030dc97205268f9d4777e16bfcc1313
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-10-04 12:32:12 +0200

    Added welcome data (sync on join).

--- index.js
+++ index.js
@@ -5,19 +5,48 @@ const app = require('express')();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let rooms = {};
+
 io.on('connection', socket => {
     console.log(`[+] ${socket.id}`);
 
+    let roomID = '';
+
+    socket.on('join', ({room}) => {
+        roomID = room;
+
+        if (rooms[room] === undefined)
+            rooms[room] = {url: '', playing: true, seconds: 0, lastUpdated: Date.now()};
+
+        socket.join(room);
+
+        socket.emit('welcome', rooms[room]);
+    });
+
     socket.on('start', data => {
-        socket.broadcast.emit('start', data);
+        socket.to(roomID).broadcast.emit('start', data);
+        rooms[roomID].url = data.url;
+        rooms[roomID].playing = true;
+        rooms[roomID].seconds = 0;
+        rooms[roomID].lastUpdated = Date.now();
     });
 
     socket.on('play', data => {
-        socket.broadcast.emit('play', data);
+        socket.to(roomID).broadcast.emit('play', data);
+        rooms[roomID].playing = true;
+        rooms[roomID].seconds = data.seconds;
+        rooms[roomID].lastUpdated = Date.now();
     });
 
     socket.on('pause', data => {
-        socket.broadcast.emit('pause', data);
+        socket.to(roomID).broadcast.emit('pause', data);
+        rooms[roomID].playing = false;
+        rooms[roomID].seconds = data.seconds;
+        rooms[roomID].lastUpdated = Date.now();
+    });
+
+    socket.on('ended', () => {
+
     });
 
     socket.on('disconnect', () => {
